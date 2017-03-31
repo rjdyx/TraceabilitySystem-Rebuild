374cda555ee6456857af6e656f3d2fa3418b5da9
= <=>
2017-03-31 11:10:19 +0800

    [mod]

--- src/assets/js/utils/validate.js
+++ src/assets/js/utils/validate.js
@@ -1,72 +1,39 @@
 /**
- *  ÷ª˙ªÚπÃ∂®µÁª∞—È÷§
+ * ÊâãÊú∫ÂíåÂõ∫ËØù
  */
 exports.phones = () => {
   return {
-    getMessage: (field) => '∏Ò Ω¥ÌŒÛ',
+    getMessage: (field) => 'Ê†ºÂºèÈîôËØØ',
     validate: (value) => {
       let length = value.length
       return (length === 11 && /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(value)) || (length === 12 && /^(([0+]\d{2,3}-)?(0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/.test(value))
     }
   }
-<<<<<<< HEAD
-=======
-      getMessage: (field) => 'Ê†ºÂºèÈîôËØØ',
-      validate: (value) => {
-          let length = value.length
-          return(length == 11 && /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(value)) || (length == 12 && /^(([0\+]\d{2,3}-)?(0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/.test(value))
-		}
-	}
->>>>>>> Denton
 }
 
 /**
- * πÃ∂®µÁª∞—È÷§
+ * Âõ∫ËØù
  */
-<<<<<<< HEAD
 
-=======
->>>>>>> Denton
 exports.phone = () => {
   return {
-    getMessage: (field) => '∏Ò Ω¥ÌŒÛ',
+    getMessage: (field) => 'Ê†ºÂºèÈîôËØØ',
     validate: (value) => {
       let length = value.length
       return length === 12 && /^(([0+]\d{2,3}-)?(0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/.test(value)
-<<<<<<< HEAD
-=======
-exports.phone = () = >{
-  return {
-    getMessage: (field) =>'Ê†ºÂºèÈîôËØØ',
-    validate: (value) =>{
-      let length = value.length
-      return length == 12 && /^(([0\+]\d{2,3}-)?(0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/.test(value)
->>>>>>> Denton
     }
   }
 }
 
 /**
- *  ÷ª˙—È÷§
+ * ÊâãÊú∫
  */
 exports.cellphone = () => {
   return {
-    getMessage: (field) => '∏Ò Ω¥ÌŒÛ',
+    getMessage: (field) => 'Ê†ºÂºèÈîôËØØ',
     validate: (value) => {
       let length = value.length
       return length === 11 && /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(value)
     }
   }
 }
-<<<<<<< HEAD
-=======
-exports.cellphone = () = >{
-  return {
-    getMessage: (field) = >'Ê†ºÂºèÈîôËØØ',
-    validate: (value) = >{
-      let length = value.length;
-      return length == 11 && /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(value)
-    }
-  }
-}
->>>>>>> Denton
