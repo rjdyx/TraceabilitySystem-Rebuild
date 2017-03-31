91775f43e2c61757375283d680899d8321c05b59
= <=>
2017-03-31 15:50:54 +0800

    [mod]

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,13 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // allow 未使用的变量
+    'no-unused-vars': 0,
+    // 允许使用未声明的变量，除非在/*global */注释中提及
+    'no-undef': 0,
+    // Disallow Assignment in return Statement
+    'no-return-assign': 0,
+
   }
 }
