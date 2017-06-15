79ab2e545d23197c4e4fa9afbc0df43d408dcd9a
wuyanping <1063218918@qq.com>
2017-06-15 10:45:56 +0800

    [Add]  添加webAppNew文件

---
+++ src/assets/js/webapp/public/camera.vue
@@ -0,0 +1,98 @@
+/*获取图片组件
+* @description 
+* @author 吴燕萍
+* @date 2017/6/14
+* */ 
+<template>
+	 <div class="camera">
+        <span class="cancelImg" @click="delImgFn"><icon type="cancel" ></icon></span>
+        <div class="cameraCon" @click="selectPic">
+            <img v-if="imageUrl === ''" class="cameraImg" src="/public/images/xiangji.png" alt="">
+            <input type="file" hidden="hidden" accept="image/*;capture=camera" @change="previewPic(item, $event)">
+            <img v-if='imageUrl' :src='imageUrl' width="100%" height="100%">
+        </div>
+        
+    </div>
+</template>
+<style lang="sass">
+@import '../../../sass/mixins/_absoluteCenter.scss';
+.camera {
+    margin-top:5px;
+    height:5rem;
+    background-color:#ededed;
+    position:relative;
+    border: 1px solid #D9D9D9;
+    .cameraCon{
+        width:100%;
+        height:100%;
+        .cameraImg{
+            @include centerfn('center',absolute,15%,left,50%,top,50%)
+        }
+    }
+        
+    .cancelImg{
+        z-index:3;
+        @include centerfn('right',absolute,auto,right,.1rem,top,.1rem)
+        >i{
+        	font-weight:bold;
+        }
+    }
+}
+</style>
+<script>
+import {Icon} from 'vux'
+export default {
+    name: 'camera',
+    components: {
+        Icon
+    },
+    data () {
+        return {
+            imageUrl: '',
+            file: {},
+            pattern: {
+                type: Array,
+                default () {
+                    return ['jpeg', 'png']
+                }
+            }
+        }
+    },
+    methods: {
+        // 删除图片
+        delImgFn () {
+            console.log('ksldghlsdglsdkjbg;l')
+            this.imageUrl = ''
+            // this.$emit('return-shuju', {name: this.shuju.name, value: ''})
+        },
+        selectPic (event) {
+            // 取出空格
+            let obj = $('input[type=file]')
+            // 触发input的click事件
+            obj.click()
+        },
+        previewPic (srcPic, event) {
+            let file = event.target.files[0]
+            let regexParams = ''
+            for (let index = 0; index < this.pattern.length; index++) {
+                regexParams += this.pattern[index] + (index === this.pattern.length - 1 ? '' : '|')
+            }
+            let regex = new RegExp('(?:' + regexParams + ')', 'i')
+            if (file.type !== 'image/png' && file.type !== 'image/jpeg') {
+                this.$message('请上传文件格式为jpeg或png的图片')
+                return
+            }
+            if (file.size / 1024 >= 300) {
+                this.$message('图片过大，请输入小于300k图片')
+                return
+            }
+            let reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = e => {
+                this.imageUrl = e.target.result
+                // this.$emit('return-shuju', {name: this.shuju.name, value: file})
+            }
+        }
+    }
+}
+</script>
