598a99d1c4a70234926c68b3e935ea3aab18ab63
sdt <15626204923@163.com>
2017-06-21 11:59:40 +0800

    [mod]

--- src/assets/js/webapp/page/webAppModel/webApp-model.vue
+++ src/assets/js/webapp/page/webAppModel/webApp-model.vue
@@ -15,7 +15,7 @@
                 <tab-item v-for="(model,index) in models" 
                 @on-item-click="tabClick(index, model.tab)" 
                 :key="index" 
-                :selected="demo2===model.tab">
+                :selected="demo2===0">
                     {{model.tab}}
                 </tab-item>
             </tab>
@@ -72,11 +72,10 @@
                             <img :src="$img('images/ok.png')">
                         </span>
                         <span v-else="!protos[index]=='img'"
-                            :name="theads[index]"  
-                            :style="{width: widths[index] + '%'}">
-                                {{pers[protos[index]]}}
+                        :name="theads[index]"  
+                        :style="{width: widths[index] + '%'}">
+                            {{pers[protos[index]]}}
                         </span>
-                        
                     </div>
                     <div slot="right-menu">
                       <swipeout-button class="lookOver" type="primary" @click.native="showDetail(pers.id)" v-if="rightMenu">查看</swipeout-button>
@@ -90,7 +89,6 @@
                 <el-button type="primary" class="allcheck" @click="checkedAll">全选</el-button>
                 <el-button type="danger" class="appDelete" @click="listDelete">删除</el-button>
             </div>
-            
             <!-- 分页 -->
             <paginator :total="total" @pageEvent="pageChange"></paginator>
 
@@ -118,6 +116,7 @@ export default {
             default () {
                 return [{
                     key: '',
+                    unite: '',
                     // 时间操作
                     url: '',
                     roleName: '',
@@ -159,8 +158,7 @@ export default {
             endDate: '',
             active: true,
             ishas: true,
-            activeindex: '',
-            demo2: ''
+            activeindex: ''
         }
     },
     // 混合
@@ -208,12 +206,8 @@ export default {
         },
         tabClick (subindex, modelName) {
             this.modelIndex = subindex
-            // console.log(modelName)
-            // this.demo2 = modelName
             localStorage.setItem('appTab', modelName)
-            // console.log(this.tabIndex)
-            this.demo2 = modelName
-            localStorage.setItem('appTab', this.modelIndex)
+            localStorage.setItem('catname', modelName)
         },
         // 侧边栏的显示与隐藏
         showsider () {
@@ -223,11 +217,11 @@ export default {
             this.show = false
         },
         showDetail (id) {
-            this.$router.push('/appIndex/appdetailbasic/' + this.batch + '/' + id)
-            let fuck = localStorage.getItem('appTab')
-            this.settitle = fuck
-            console.log(this.settitle + 'why')
-            console.log(fuck + 'ddddddddddddddddddddddddddddd')
+            if (this.unite === 'plantTo') {
+                this.$router.push('/appIndex/appdetailbasic/' + this.batch + '/plantTo' + id)
+            } else {
+                this.$router.push('/appIndex/appdetailbasic/' + this.batch + '/' + id)
+            }
         },
         closeOperate () {
             if (this.ishas === true) {
@@ -367,7 +361,8 @@ export default {
         Swiper,
         SwiperItem,
         Toast,
-        Confirm
+        Confirm,
+        Popover
     }
 }
 </script>
