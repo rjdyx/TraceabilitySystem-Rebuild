9cfe6751b9963789579970e014f64a98ee9f1485
limingcun <1521010816@qq.com>
2017-07-24 16:00:17 +0800

    [ADD]添加inputDateTimes.vue

---
+++ src/assets/js/components/public/inputDateTimes.vue
@@ -0,0 +1,42 @@
+/**
+ * inputData组件
+ * @description 
+ * @author 吴燕萍
+ * @date 2017/3/21
+
+ */
+<template>
+    <el-date-picker
+      v-model="value"
+      :type="type"
+      placeholder="选择时间范围"
+      size="small">
+    </el-date-picker>
+</template>
+<script>
+    export default {
+        props: {
+            shuju: {
+                type: Object,
+                default () {
+                    return {}
+                }
+            },
+            type: ''
+        },
+        data () {
+            return {
+                value: ''
+            }
+        },
+        methods: {
+        },
+        watch: {
+            value () {
+                this.$emit('return-shuju', {name: this.shuju.name, value: this.value})
+            }
+        },
+        mounted () {
+        }
+    }
+</script>
