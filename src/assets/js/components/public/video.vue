c85c018c7c0ebb4470c0892e4242659ff096511e
limingcun <1521010816@qq.com>
2017-08-23 17:41:26 +0800

    [MOD]

--- src/assets/js/components/public/video.vue
+++ src/assets/js/components/public/video.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="inputVideo">
+    <!-- <div class="inputVideo" id="picker">
         <button class="el-button el-button--default el-button--small">{{tip}}
             <input type="file" accept="audio/mp4, video/mp4" @change="changefn($event.currentTarget, $event)">
         </button>
         <i v-if="value==''" class="tipIcon el-icon-circle-close"></i>
         <i v-else class="tipIcon el-icon-circle-check"></i>
         <el-button size="small" @click="upVideo()" class="btn_change delVideo">上传</el-button>
+    </div> -->
+    <div id="uploader">
+        <ul id="theList"></ul>
+        <div id="picker">选择文件</div>
     </div>
 </template>
 <script type="text/javascript"></script>
@@ -33,21 +37,311 @@ export default {
         }
     },
     methods: {
-        changefn (srcPic, event) {
-            let file = event.target.files[0]
-            if (file.size / 1024 >= 614400) {
-                this.$message('视频文件过大，请输入小于600M视频')
-                srcPic.value = ''
-                return
+        abc () {
+            var userInfo = {userId: 'kazaff666', md5: ''}
+            var chunkSize = 5000 * 1024
+            var uniqueFileName = null
+            var md5Mark = null
+            var backEndUrl = '/php/fileUpload.php'
+            WebUploader.Uploader.register({
+                'before-send-file': 'beforeSendFile',
+                'before-send': 'beforeSend',
+                'after-send-file': 'afterSendFile'
+            }, {
+                beforeSendFile: function (file) {
+                    var task = new $.Deferred()
+                    var start = new Date().getTime()
+                    new WebUploader.Uploader().md5File(file, 0, 10 * 1024 * 1024).progress(function (percentage) {
+                    }).then(function (val) {
+                        md5Mark = val
+                        userInfo.md5 = val
+                        $.ajax({
+                            type: 'POST',
+                            url: backEndUrl,
+                            data: {
+                                status: 'md5Check',
+                                md5: val
+                            },
+                            cache: false,
+                            // todo 超时的话，只能认为该文件不曾上传过
+                            timeout: 1000,
+                            dataType: 'json'
+                        }).then(function (data, textStatus, jqXHR) {
+                            // 若存在，这返回失败给WebUploader，表明该文件不需要上传
+                            if (data.ifExist) {
+                                task.reject()
+                                uploader.skipFile(file)
+                                file.path = data.path
+                                UploadComlate(file)
+                            } else {
+                                task.resolve()
+                                // 拿到上传文件的唯一名称，用于断点续传
+                                uniqueFileName = md5('' + userInfo.userId + file.name + file.type + file.lastModifiedDate + file.size)
+                            }
+                        // 任何形式的验证失败，都触发重新上传
+                        }, function (jqXHR, textStatus, errorThrown) {
+                            task.resolve()
+                            // 拿到上传文件的唯一名称，用于断点续传
+                            uniqueFileName = md5('' + userInfo.userId + file.name + file.type + file.lastModifiedDate + file.size)
+                        })
+                    })
+                    return $.when(task)
+                },
+                beforeSend: function (block) {
+                    // 分片验证是否已传过，用于断点续传
+                    var task = new $.Deferred()
+                    $.ajax({
+                        type: 'POST',
+                        url: backEndUrl,
+                        data: {
+                            status: 'chunkCheck',
+                            name: uniqueFileName,
+                            chunkIndex: block.chunk,
+                            size: block.end - block.start
+                        },
+                        cache: false,
+                        timeout: 1000,
+                        dataType: 'json'
+                    }).then(function (data, textStatus, jqXHR) {
+                        if (data.ifExist) {
+                            task.reject()
+                        } else {
+                            task.resolve()
+                        }
+                    }, function (jqXHR, textStatus, errorThrown) {
+                        task.resolve()
+                    })
+                    return $.when(task)
+                },
+                afterSendFile: function (file) {
+                    var chunksTotal = 0
+                    if ((chunksTotal = Math.ceil(file.size / chunkSize)) > 1) {
+                        // 合并请求
+                        var task = new $.Deferred()
+                        $.ajax({
+                            type: 'POST',
+                            url: backEndUrl,
+                            data: {
+                                status: 'chunksMerge',
+                                name: uniqueFileName,
+                                chunks: chunksTotal,
+                                ext: file.ext,
+                                md5: md5Mark
+                            },
+                            cache: false,
+                            dataType: 'json'
+                        }).then(function (data, textStatus, jqXHR) {
+                            // todo 检查响应是否正常
+                            task.resolve()
+                            file.path = data.path
+                            UploadComlate(file)
+                        }, function (jqXHR, textStatus, errorThrown) {
+                            task.reject()
+                        })
+                        return $.when(task)
+                    } else {
+                        UploadComlate(file)
+                    }
+                }
+            })
+            var uploader = WebUploader.create({
+                swf: this.$img('webuploader/Uploader.swf'),
+                server: backEndUrl,
+                pick: '#picker',
+                resize: false,
+                compress: false,
+                prepareNextFile: true,
+                chunked: true,
+                chunkSize: chunkSize,
+                threads: true,
+                formData: function () {
+                    return $.extend(true, {}, userInfo)
+                },
+                fileNumLimit: 1,
+                fileSingleSizeLimit: 1000 * 1024 * 1024,
+                duplicate: true
+            })
+            uploader.on('fileQueued', function (file) {
+                $('#theList').append('<li id=' + file.id + '>' +
+                    '<img /><span>' + file.name + '</span><span class=itemUpload>上传</span><span class=itemStop>暂停</span><span class=itemDel>删除</span>' +
+                    '<div class=percentage></div>' +
+                '</li>')
+                var $img = $('#' + file.id).find('img')
+                $('.itemStop').hide()
+                uploader.makeThumb(file, function (error, src) {
+                    if (error) {
+                        $img.replaceWith('<span>不能预览</span>')
+                    }
+                    $img.attr('src', src)
+                })
+            })
+            $('#theList').on('click', '.itemUpload', function () {
+                uploader.upload()
+                // "上传"-->"暂停"
+                $(this).hide()
+                $('.itemStop').show()
+            })
+            $('#theList').on('click', '.itemStop', function () {
+                uploader.stop(true)
+                // "暂停"-->"上传"
+                $(this).hide()
+                $('.itemUpload').show()
+            })
+            // todo 如果要删除的文件正在上传（包括暂停），则需要发送给后端一个请求用来清除服务器端的缓存文件
+            $('#theList').on('click', '.itemDel', function () {
+                uploader.removeFile($(this).parent().attr('id'))
+                // 从上传列表dom中删除
+                $(this).parent().remove()
+            })
+            uploader.on('uploadProgress', function (file, percentage) {
+                $('#' + file.id + ' .percentage').text(percentage * 100 + '%')
+            })
+            function UploadComlate (file) {
+                $('#' + file.id + ' .percentage').text('上传完毕')
+                $('.itemStop').hide()
+                $('.itemUpload').hide()
+                $('.itemDel').hide()
             }
-            this.value = $('.inputVideo input[type=file]').eq(0).val()
-            this.val = file
+        },
+        changefn (srcPic, event) {
+            // let file = event.target.files[0]
+            // if (file.size / 1024 >= 614400) {
+            //     this.$message('视频文件过大，请输入小于600M视频')
+            //     srcPic.value = ''
+            //     return
+            // }
+            // var userInfo = {userId: 'kazaff666', md5: ''}
+            // var chunkSize = 5000 * 1024
+            // var uniqueFileName = null
+            // var md5Mark = null
+            // WebUploader.Uploader.register({
+            //     'before-send-file': 'beforeSendFile',
+            //     'before-send': 'beforeSend',
+            //     'after-send-file': 'afterSendFile'
+            // }, {
+            //     beforeSendFile: function (file) {
+            //         var task = new $.Deferred()
+            //         var start = new Date().getTime()
+            //         new WebUploader.Uploader().md5File(file, 0, 10 * 1024 * 1024).progress(function (percentage) {
+            //         }).then(function (val) {
+            //             md5Mark = val
+            //             userInfo.md5 = val
+            //             $.ajax({
+            //                 type: 'POST',
+            //                 url: '/upLoadVideo',
+            //                 data: {
+            //                     status: 'md5Check',
+            //                     md5: val
+            //                 },
+            //                 cache: false,
+            //                 // todo 超时的话，只能认为该文件不曾上传过
+            //                 timeout: 1000,
+            //                 dataType: 'json'
+            //             }).then(function (data, textStatus, jqXHR) {
+            //                 // 若存在，这返回失败给WebUploader，表明该文件不需要上传
+            //                 if (data.ifExist) {
+            //                     task.reject()
+            //                     uploader.skipFile(file)
+            //                     file.path = data.path
+            //                     UploadComlate(file)
+            //                 } else {
+            //                     task.resolve()
+            //                     // 拿到上传文件的唯一名称，用于断点续传
+            //                     uniqueFileName = md5('' + userInfo.userId + file.name + file.type + file.lastModifiedDate + file.size)
+            //                 }
+            //             // 任何形式的验证失败，都触发重新上传
+            //             }, function (jqXHR, textStatus, errorThrown) {
+            //                 task.resolve()
+            //                 // 拿到上传文件的唯一名称，用于断点续传
+            //                 uniqueFileName = md5('' + userInfo.userId + file.name + file.type + file.lastModifiedDate + file.size)
+            //             })
+            //         })
+            //         return $.when(task)
+            //     },
+            //     beforeSend: function (block) {
+            //         // 分片验证是否已传过，用于断点续传
+            //         var task = new $.Deferred()
+            //         $.ajax({
+            //             type: 'POST',
+            //             url: '/upLoadVideo',
+            //             data: {
+            //                 status: 'chunkCheck',
+            //                 name: uniqueFileName,
+            //                 chunkIndex: block.chunk,
+            //                 size: block.end - block.start
+            //             },
+            //             cache: false,
+            //             timeout: 1000,
+            //             dataType: 'json'
+            //         }).then(function (data, textStatus, jqXHR) {
+            //             if (data.ifExist) {
+            //                 task.reject()
+            //             } else {
+            //                 task.resolve()
+            //             }
+            //         }, function (jqXHR, textStatus, errorThrown) {
+            //             task.resolve()
+            //         })
+            //         return $.when(task)
+            //     },
+            //     afterSendFile: function (file) {
+            //         var chunksTotal = 0
+            //         if ((chunksTotal = Math.ceil(file.size / chunkSize)) > 1) {
+            //             // 合并请求
+            //             var task = new $.Deferred()
+            //             $.ajax({
+            //                 type: 'POST',
+            //                 url: '/upLoadVideo',
+            //                 data: {
+            //                     status: 'chunksMerge',
+            //                     name: uniqueFileName,
+            //                     chunks: chunksTotal,
+            //                     ext: file.ext,
+            //                     md5: md5Mark
+            //                 },
+            //                 cache: false,
+            //                 dataType: 'json'
+            //             }).then(function (data, textStatus, jqXHR) {
+            //                 // todo 检查响应是否正常
+            //                 task.resolve()
+            //                 file.path = data.path
+            //                 UploadComlate(file)
+            //             }, function (jqXHR, textStatus, errorThrown) {
+            //                 task.reject()
+            //             })
+            //             return $.when(task)
+            //         } else {
+            //             UploadComlate(file)
+            //         }
+            //     }
+            // })
+            // var uploader = WebUploader.create({
+            //     swf: this.$img('webuploader/Uploader.swf'),
+            //     server: '/upLoadVideo',
+            //     pick: '#picker',
+            //     resize: false,
+            //     compress: false,
+            //     prepareNextFile: true,
+            //     chunked: true,
+            //     chunkSize: chunkSize,
+            //     threads: true,
+            //     formData: function () {
+            //         return $.extend(true, {}, userInfo)
+            //     },
+            //     fileNumLimit: 1,
+            //     fileSingleSizeLimit: 1000 * 1024 * 1024,
+            //     duplicate: true
+            // })
+            // uploader.on('fileQueued', function (file) {
+            //     console.log(777)
+            // })
+            // this.value = $('.inputVideo input[type=file]').eq(0).val()
+            // this.val = file
         },
         upVideo () {
             if (this.val !== '') {
-                Promise.all([this.getProcess(), this.ps()])
-                .then(([res1, res2]) => {
-                    console.dir([res1, res2])
+                uploader.on('fileQueued', function (file) {
+                    console.log(777)
                 })
             } else {
                 this.$message('请上传MP4视频文件')
@@ -82,6 +376,7 @@ export default {
         }
     },
     mounted () {
+        this.abc()
         if (this.editValue !== undefined && this.editValue !== null && this.editValue !== '') {
             this.value = this.editValue
             this.$emit('return-shuju', {name: 'videos', value: this.editValue})
@@ -93,7 +388,9 @@ export default {
     }
 }
 </script>
+<link rel="stylesheet" href="webuploader.css" />
 <style lang="sass">
+@import '../../../../../public/webuploader/webuploader.css';
 .inputVideo{
 	overflow:hidden;
 	button:first-child{
@@ -112,5 +409,18 @@ export default {
 	.tipIcon{
 		margin-left:20px;
 	}
+    .itemDel, .itemStop, .itemUpload{
+        margin-left: 15px;
+        color: blue;
+        cursor: pointer;
+    }
+    #theList{
+        width: 80%;
+        min-height: 100px;
+        border: 1px solid red;
+    }
+    #theList .itemStop{
+        display: none;
+    }
 }
 </style>
