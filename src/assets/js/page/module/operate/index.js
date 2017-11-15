e97148a5fda949abdfeb6f9ae91eb9470c1e3244
limingcun <1521010816@qq.com>
2017-11-15 16:09:35 +0800

    [MOD]修改

--- src/assets/js/page/module/operate/index.js
+++ src/assets/js/page/module/operate/index.js
@@ -2,13 +2,11 @@ import {rightsOperate} from './rightsOperate.js'
 import {settleOperate} from './settleOperate.js'
 import {usersOperate} from './usersOperate.js'
 import {logOperate} from './logOperate.js'
-import {applyOperate} from './applyOperate.js'
 
 // 整合 运营管理 全部子类
 export let operateModel = Object.assign({},
     rightsOperate,
     settleOperate,
     usersOperate,
-    logOperate,
-    applyOperate
+    logOperate
 )
